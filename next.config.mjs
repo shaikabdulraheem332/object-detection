19cf60f7ef3d52140d88a82ffdd0928f6ca61914
Developer <developer@example.com>
2026-08-10 19:41:24 +0530

    Configure GitHub Pages deployment workflow and static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const repoName = 'object-detection';
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubActions ? `/${repoName}` : '',
+  assetPrefix: isGithubActions ? `/${repoName}/` : '',
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
